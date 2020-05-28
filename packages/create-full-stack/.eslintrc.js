5c89a6320ceae021d80c2323ba860acbf3fbb113
tiagob <tiago.bndr@gmail.com>
2020-05-27 17:29:51 -0700

    Prettier configuration and eslint for create-full-stack

--- packages/create-full-stack/.eslintrc.js
+++ packages/create-full-stack/.eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: "eslint-config-create-full-stack",
   rules: {
     "unicorn/no-process-exit": "off",
-    "no-console": "off"
-  }
-}
+    "no-console": "off",
+  },
+};
--- packages/create-full-stack/templates/hasura/no-auth/packages/web/.eslintrc.js
+++ packages/create-full-stack/templates/hasura/no-auth/packages/web/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   extends: ["react-app", "eslint-config-create-full-stack"],
-  ignorePatterns: ["react-app-env.d.ts", "serviceWorker.ts", "setupTests.ts"]
-}
+  ignorePatterns: ["react-app-env.d.ts", "serviceWorker.ts", "setupTests.ts"],
+};
