fdb449a0315046e713be2eea58828be27c0f667a
tiagob <tiago.bndr@gmail.com>
2020-09-24 23:28:34 -0700

    Remove incorrect console output from Appollo Server Express

--- packages/cfs-template-apollo-server-express-auth0/packages/server/src/index.ts
+++ packages/cfs-template-apollo-server-express-auth0/packages/server/src/index.ts
@@ -1,10 +1,8 @@
 import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
-import * as dns from "dns";
 import express from "express";
 import jwt from "express-jwt";
 import jwksRsa from "jwks-rsa";
-import * as os from "os";
 import { Connection, createConnection, QueryFailedError } from "typeorm";
 
 import getResolvers, { DecodedJwt } from "./getResolvers";
@@ -68,14 +66,10 @@ async function run() {
   });
   server.applyMiddleware({ app });
 
-  dns.lookup(os.hostname(), async (_, localIp) => {
-    const port = process.env.PORT || 8080;
-    await app.listen(port);
-    // eslint-disable-next-line no-console
-    console.log(
-      `🚀  Server ready at http://localhost:${port}/graphql and http://${localIp}:${port}/graphql`
-    );
-  });
+  const port = process.env.PORT || 8080;
+  await app.listen(port);
+  // eslint-disable-next-line no-console
+  console.log(`🚀  Server ready at http://localhost:${port}/graphql`);
 }
 
 run();
--- packages/cfs-template-apollo-server-express-no-auth/packages/server/src/index.ts
+++ packages/cfs-template-apollo-server-express-no-auth/packages/server/src/index.ts
@@ -1,8 +1,6 @@
 import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
-import * as dns from "dns";
 import express from "express";
-import * as os from "os";
 import { Connection, createConnection, QueryFailedError } from "typeorm";
 
 import getResolvers from "./getResolvers";
@@ -35,14 +33,10 @@ async function run() {
   });
   server.applyMiddleware({ app });
 
-  dns.lookup(os.hostname(), async (_, localIp) => {
-    const port = process.env.PORT || 8080;
-    await app.listen(port);
-    // eslint-disable-next-line no-console
-    console.log(
-      `🚀  Server ready at http://localhost:${port}/graphql and http://${localIp}:${port}/graphql`
-    );
-  });
+  const port = process.env.PORT || 8080;
+  await app.listen(port);
+  // eslint-disable-next-line no-console
+  console.log(`🚀  Server ready at http://localhost:${port}/graphql`);
 }
 
 run();
