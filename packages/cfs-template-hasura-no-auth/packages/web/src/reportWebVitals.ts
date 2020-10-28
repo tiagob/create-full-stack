38e30608e5a25a6ffb1d412ca06ed7acff98a637
tiagob <tiago.bndr@gmail.com>
2020-10-28 10:41:56 -0700

    CRA v4

---
+++ packages/cfs-template-hasura-no-auth/packages/web/src/reportWebVitals.ts
@@ -0,0 +1,16 @@
+import { ReportHandler } from "web-vitals";
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    // eslint-disable-next-line promise/catch-or-return, promise/always-return
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
